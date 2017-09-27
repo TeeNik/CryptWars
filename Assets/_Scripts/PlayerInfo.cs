cefd7a5bb685dbffc97fba59c78f26b2386bc140
TeeNik <teenik3315@gmail.com>
2017-09-28 02:49:04 +0300

    player and resource

---
+++ Assets/_Scripts/PlayerInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour {
+
+    int coin;
+    int energy;
+
+    [SerializeField]
+    float maxHealth;
+
+    float curHealth;
+
+}
---
+++ Assets/_Scripts/ResourceManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] characters;
+
+
+
+
+
+    public static Get<T>(string name){
+
+    }
+
+
+
+}
