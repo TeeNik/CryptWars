1d83c3ea2fceb9171b5139750bf833b9ae65d776
TeeNik <teenik3315@gmail.com>
2017-10-15 14:32:39 +0300

    15.10

--- Assets/_Scripts/PlayerInfo.cs
+++ Assets/_Scripts/PlayerInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInfo : MonoBehaviour {
 
@@ -10,6 +11,19 @@ public class PlayerInfo : MonoBehaviour {
     [SerializeField]
     float maxHealth;
 
+    [SerializeField]
+    Image heathBar;
+
     float curHealth;
+
+    private void Start()
+    {
+        curHealth = maxHealth-20;
+    }
+
+    private void Update()
+    {
+        heathBar.fillAmount = curHealth / maxHealth;
+    }
 
 }
