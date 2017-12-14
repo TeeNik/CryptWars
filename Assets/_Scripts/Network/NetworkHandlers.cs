d51e672e3bb19354371398bc9d58b9bf0980932a
TeeNik <teenik3315@gmail.com>
2017-12-15 02:05:23 +0300

    15.12

--- Assets/_Scripts/Network/NetworkHandlers.cs
+++ Assets/_Scripts/Network/NetworkHandlers.cs
@@ -7,19 +7,19 @@ using UnityEngine;
 
 public class ResponseObject
 {
-    public string eventName;
-    public JSONObject jsonObject;
+    public string EventName;
+    public JSONObject JsonObject;
 }
 
 public class NetworkHandlers : MonoBehaviour {
 
-    private static NetworkManager networkManager;
+    private static NetworkManager _networkManager;
     private static List<ResponseObject> responseObjects = new List<ResponseObject>();
 
     public static void Init(NetworkManager n)
     {
-        networkManager = n;
-        networkManager.responceEvent = OnEvent;
+        _networkManager = n;
+        _networkManager.responceEvent = OnEvent;
     }
 
     public static void FixedUpdate()
@@ -28,16 +28,16 @@ public class NetworkHandlers : MonoBehaviour {
         {
             if (responseObjects[i] != null)
             {
-                NetworkCommands eventType = responseObjects[i].eventName.ToEnum<NetworkCommands>();
+                NetworkCommands eventType = responseObjects[i].EventName.ToEnum<NetworkCommands>();
                 switch (eventType)
                 {
                     case NetworkCommands.auth:
                         break;
                     case NetworkCommands.connect:
 
-                        ConnectObject co = ConnectObject.FromJson<ConnectObject>(responseObjects[i].jsonObject.ToString());
+                        ConnectObject co = ConnectObject.FromJson<ConnectObject>(responseObjects[i].JsonObject.ToString());
                         //networkManager.Send(NetworkCommands.connect.ToString(), ConnectObject.FromJson<ConnectObject>(re)
-                        print(responseObjects[i].jsonObject.ToString());
+                        print(responseObjects[i].JsonObject.ToString());
                         print(co.id + "  " + co.ok);
                         break;
                     case NetworkCommands.startBattle:
@@ -60,15 +60,15 @@ public class NetworkHandlers : MonoBehaviour {
         responseObjects.Add(
             new ResponseObject()
             {
-                eventName = ev,
-                jsonObject = json
+                EventName = ev,
+                JsonObject = json
             }
         );
     } 
 
     private void SpawnEvent(ResponseObject r)
     {
-        WarriorObject w = WarriorObject.FromJson<WarriorObject>(r.jsonObject.ToString());
+        WarriorObject w = WarriorObject.FromJson<WarriorObject>(r.JsonObject.ToString());
         EventHandler.instance.spawnEvent(w);
     }
 }
